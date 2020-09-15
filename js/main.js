28e9e1f582a8b06b771d8e35b5f16fc3dbaa56e1
markkalalo <kalalomark5@gmail.com>
2020-09-16 05:46:14 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// jQuery(document).ready(function(){
+    
+//     $("#navbarNav ul li a[href^='#']").on('click', function(e) {
+//         e.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: $(this.hash).offset().top
+//         }, 2000, function(){});
+//     });
+
+// });
