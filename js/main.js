b6a9761678c8baf9997a70eeb8a07fda71ac2fc6
markkalalo <kalalomark5@gmail.com>
2020-09-17 15:20:34 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,10 @@
-jQuery(document).ready(function(){
+// jQuery(document).ready(function(){
     
-    $("#navbarNav ul li a[href^='#']").on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 2000, function(){});
-    });
+//     $("#navbarNav ul li a[href^='#']").on('click', function(e) {
+//         e.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: $(this.hash).offset().top
+//         }, 2000, function(){});
+//     });
 
-    // $('.slider-for').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     fade: true,
-    //     asNavFor: '.slider-nav'
-    //   });
-    //   $('.slider-nav').slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.slider-for',
-    //     dots: true,
-    //     focusOnSelect: true
-    //   });
-     
-    //   $('a[data-slide]').click(function(e) {
-    //     e.preventDefault();
-    //     var slideno = $(this).data('slide');
-    //     $('.slider-nav').slick('slickGoTo', slideno - 1);
-    //   });
-
-});
+// });
