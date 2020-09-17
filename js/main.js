0ef48e736e522048b305f3fdec67603ed54a3b01
markkalalo <kalalomark5@gmail.com>
2020-09-17 19:59:37 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-// jQuery(document).ready(function(){
-    
-//     $("#navbarNav ul li a[href^='#']").on('click', function(e) {
-//         e.preventDefault();
-//         $('html, body').animate({
-//             scrollTop: $(this.hash).offset().top
-//         }, 2000, function(){});
-//     });
+jQuery(document).ready(function(){
 
-// });
+    var browserWindow = $(window);
+
+    browserWindow.on('load', function() {
+        $('.preloader').fadeOut('slow', function() {
+        $(this).remove();
+        });
+    });
+    
+    $("#navbarNav ul li a[href^='#']").on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 2000, function(){});
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            "<i class='fa fa-caret-left'></i>",
+            "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        items : 1,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+});
